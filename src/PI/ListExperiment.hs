6272d8abbd9ce9307e9e8483e19dbef491107d43
Eyal Dechter <edechter@mit.edu>
2013-01-03 11:58:44 -0500

    Added ListExperiment.hs. File will contain tasks and experiment structure for list function learning experiment.

---
+++ src/PI/ListExperiment.hs
@@ -0,0 +1,51 @@
+-- ListExperiment.hs
+
+module ListExperiment where 
+
+import Control.Monad
+import Debug.Trace
+
+import Expr
+import StdLib
+import Data
+import CL
+import CombMap (CombMap)
+import Task
+import Type
+import Experiment
+
+
+-- Basic List Experiment --
+
+
+mkIntList :: [Int] -> SynthComb
+mkIntList (x:xs) = Right cCons <:> Right (num2C x) <:> mkIntList xs
+mkIntList [] = Right cEmpty
+
+maybe2Either :: Maybe a -> Either String a
+maybe2Either (Just a) = Right a
+maybe2Either Nothing = Left ""
+
+fromRight (Right x) = x
+
+tLast = Task "first" f tp
+    where tp = (TAp tList tInt) ->- tInt
+          f comb = reward
+              where ex = (trace $ show comb) $ liftM comb2Expr $ Right comb <:> (mkIntList [1,2,3,4]) 
+                    result = reduce (fromRight ex)
+                    reward = case result of 
+                               (N 1) -> 1
+                               otherwise -> 0
+
+expTList1 = Experiment {
+              expName = "List function: first",
+              expTaskSet = [tLast],
+              expEps = 0,
+              expPrior = stdlib,
+              expInitLib = stdlib,
+              expLogLikeBound = (-6),
+              expDepthBound = 3,
+              expDataType = (tInt),
+              expReps = 10}
+                      
+                   
