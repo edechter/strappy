dbeb9b918a790a3e19591841ba7d150405ab3b5c
Eyal Dechter <edechter@mit.edu>
2013-01-04 12:41:12 -0500

    Added estimateGrammar method (ticket #8).

--- src/PI/Grammar.hs
+++ src/PI/Grammar.hs
@@ -2,13 +2,14 @@
 
 module Grammar where
 
-import CL (show')
+import CL 
 import qualified CombMap as CM
 import CombMap (CombMap)
+import Compress
 
 
 data Grammar = Grammar { library :: CombMap Int,
-                         expandCost :: Int} 
+                         expansions :: Int} 
 
 showLibrary :: Show a => CombMap a  -> String
 showLibrary ct = unlines $ map (\(c, i) -> show i ++ ": " ++ 
@@ -16,8 +17,23 @@ showLibrary ct = unlines $ map (\(c, i) -> show i ++ ": " ++
 
 instance Show Grammar where
     show (Grammar lib c) = "Grammar\n------------------------" 
-                           ++ "\nExpansions cost: " ++ show c
+                           ++ "\nExpansions: " ++ show c
                            ++ "\n Library: " 
                            ++ showLibrary lib
+
+estimateGrammar :: [Comb] -> Grammar
+-- | Generate a new grammar from a set of combinators using the
+-- combinator tree compression scheme defined in Compress.hs
+-- (compress). Include all trees from the compression that occur more
+-- than once (i.e. Sequitur / Neville-Manning algorithm). To estimate
+-- the expansions, take the total number of trees in from the
+-- compression and subtract the number of input combinators.
+estimateGrammar cs = Grammar lib c
+    where ind = compress cs
+          xs = CM.assocs ind
+          count = CM.fold (+) 0 ind -- sum of tree counts in index
+          lib = CM.fromList $ filter ((>1) . snd) xs 
+          c = count - length cs
+          
     
     
