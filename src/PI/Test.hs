832542af9b7a46fd287cff0240a665f999f23563
Eyal Dechter <edechter@mit.edu>
2013-01-04 14:31:13 -0500

    Cleanup.

--- src/PI/Test.hs
+++ src/PI/Test.hs
@@ -18,29 +18,6 @@ import Experiment
 import qualified Compress as CP
 import ListExperiment
 
--- xs = fst 
---      $ runSearch 
---      $ findCombinatorsForEachDatum expIntegerSequences stdlibTrie 
--- xs' = filter (\x -> (length $ snd x) > 0)  xs
--- (index, rs) = greedyN 5 (sortData xs')
--- index' = newLibrary $ map snd rs
-
 grammar = fst $ runSearch $ loop expTList1
---y = map reduceComb$ map fst $ runStateT (enum (CM.keys stdlib) 3 tInt) 0
 main = do
---   let lib = stdlibTrie
---       c = (map runTI $ enumCombsToProb lib (-10) 3 Rtype)
---   putStrLn $ show $ map ((reduceWithLimit rlimit . comb2Expr') . fst) c
-
---   putStrLn "Results: "
---   let results = loop expIntegerSequences
---       ks = CT.keys results
---       vs = CT.toList results
---       out1 = [ show v ++ ": " ++ show' k | k <- ks | v <- vs]
---   putStrLn $ unlines out1
    putStrLn $ show grammar    
---   putStrLn $ unlines $ map (\(c, n) -> show' c ++ ": " ++ show n) (CM.assocs index)
---   putStrLn $ unlines $ map (\(c, n) -> show' n ++ ": " ++ show c) rs
---   putStrLn $ unlines $ map (\(c, n) -> show' c ++ ": " ++ show n) (CT.toAscList index')
-
---putStrLn $ show y
