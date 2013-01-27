e5d8c9d627c9485de692182eb25dce4f975e3789
Eyal Dechter <edechter@mit.edu>
2013-01-27 18:42:25 -0500

    Minor changes.

--- src/PI/Experiments/EX2.hs
+++ src/PI/Experiments/EX2.hs
@@ -12,15 +12,15 @@ main = do
   tasks <- readTasksFromCircuits "data/EX2/circuitTasks.txt"
   exp1000 <- mkBoolCircuitsExperiment "ex2_circuitlearning_1000"
                lib1
-                10
+                1000
                 tasks
   exp5000 <- mkBoolCircuitsExperiment "ex2_circuitlearning_5000"
                lib1
-                10
+                5000
                 tasks
   exp10000 <-mkBoolCircuitsExperiment "ex2_circuitlearning_10000"
                lib1
-                10
+                10000
                 tasks
   sequence $ map (\x -> runExp x "data/EX2") [exp1000, exp5000, exp10000]
   return ()
