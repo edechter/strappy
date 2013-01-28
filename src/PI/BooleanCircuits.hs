5f2e90fb73420e3812bb6a43ada0f6b6bd2b97c5
Eyal Dechter <edechter@mit.edu>
2013-01-27 19:11:22 -0500

    Changes.

--- src/PI/BooleanCircuits.hs
+++ src/PI/BooleanCircuits.hs
@@ -252,8 +252,8 @@ sampleCircuitTasks dI dN dG nT
           
 
 -- DISTRIBUTIONS -- 
-dNumInputs = [(1, 1), (2, 3), (3, 4)] :: [(Int, Rational)]
-dNumGates = [(1, 1), (2, 3), (3,  4), (4, 4), (5, 3)] :: [(Int, Rational)]
+dNumInputs = [(1, 1), (2, 2), (3, 4)] :: [(Int, Rational)]
+dNumGates = [(1, 1), (2, 3), (3,  4), (4, 5), (5, 4)] :: [(Int, Rational)]
 dGateType = [ (MGate notGate, 1), (BGate andGate, 2), (BGate orGate, 2)] :: [(Gate, Rational)]
 
 
