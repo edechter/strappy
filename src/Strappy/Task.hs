fe1b5b2418498dd55f0930902a9363293e607378
Eyal Dechter <edechter@mit.edu>
2013-05-22 19:02:49 -0400

    Removing old stuff.

--- src/Strappy/Task.hs
+++ src/Strappy/Task.hs
@@ -4,42 +4,15 @@
 module Strappy.Task where
 
 import Strappy.Expr
-import Strappy.CL
 import Strappy.Type
 
-type Reward = Double
 data Task =  Task {taskName :: String, 
-                   task :: (Comb -> Reward),
+                   task :: UExpr -> Double,
                    taskType :: Type} 
-          | SymRegTask {taskName :: String,
-                        taskVals :: [Int],
-                        taskType :: Type}
-          | DigArithTask {taskName :: String,
-                          taskBoolVals :: [Bool],
-                          taskCard :: Int,
-                          taskType :: Type}
 
 type TaskSet = [Task]
 
 instance Show Task where
     show (Task n _ _ ) = n
-    show (SymRegTask n _ _ ) = n
-    show (DigArithTask n _ _ _ ) = n
 
-
--- | task constructors
-
-mkSingleEqualityTask:: Int -> Int -> Task
-mkSingleEqualityTask rlimit i 
-    = let f c = fromIntegral $ (abs $ a - i)
-              where a  = case  reduceComb c of
-                           (N a) -> a
-                           otherwise -> maxBound
-      in Task (show i) f tInt
-
-showTaskCombAssignments :: [(Task, Comb)] -> String
-showTaskCombAssignments = unlines . map f
-    where f (t, c) = show t ++ ": " ++ show c
-
-    
-
+	
