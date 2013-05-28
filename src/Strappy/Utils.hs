6ef934c62af833549fb9a590ef4a2ed3b0dff436
Eyal Dechter <edechter@mit.edu>
2013-05-28 17:31:57 -0400

    Added logsumexp-specific error message to logsumexp method. Also, a cosmetic change.

--- src/Strappy/Utils.hs
+++ src/Strappy/Utils.hs
@@ -3,6 +3,7 @@ module Strappy.Utils where
 
 import Prelude hiding (flip)
 import Control.Monad.Random 
+import Data.List (null)
 
 flip :: (Num a, Ord a, Random a, MonadRandom m) => a -> m Bool
 flip p = do r <- getRandomR (0, 1)
@@ -11,17 +12,17 @@ flip p = do r <- getRandomR (0, 1)
 sampleMultinomial :: (Num a, Ord a, Random a, MonadRandom m) => [(b, a)] -> m b
 sampleMultinomial dist = do r <- getRandomR (0, 1)
                             return $ sample r dist
-    where sample r ((a, p):rest) = if r <= p then a else 
-                                       sample (r - p) rest
+    where sample r ((a, p):rest) = if r <= p then a 
+                                             else sample (r - p) rest
           
-
-
-logsumexp xs = a + (log . sum . map (exp . (\x -> x-a)) $ xs)
-    where a = maximum xs
+logsumexp xs = a + (log . sum . map (exp . (\x -> x - a)) $ xs)
+    where a = if null xs then error "logsumexp: list argument must have length greater than zero, but got []." 
+                         else maximum xs
 
 normalizeDist dist
     = let logTotalMass = logsumexp (map snd dist)
       in [(c, v - logTotalMass) | (c, v) <- dist]
+
 
 
 
