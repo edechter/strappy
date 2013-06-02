acbd9083ceb6f70754c5755115d8f5ebf2d354ad
Kevin Ellis <ellisk@fiibar.(none)>
2013-06-02 18:05:35 -0400

    Added Core.hs

---
+++ src/Strappy/Core.hs
@@ -0,0 +1,11 @@
+
+-- | This module imports all of the important bits of Strappy
+
+module Core where
+
+import Strappy.Type
+import Strappy.Expr
+import Strappy.Library
+import Strappy.Task
+import Strappy.Sample
+import Strappy.Utils
