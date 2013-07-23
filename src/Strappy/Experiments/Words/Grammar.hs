c879d54ddf77b245f7440257e793042611c9dc32
Eyal Dechter <edechter@mit.edu>
2013-07-23 17:48:58 -0400

    Created a grammar file Grammar.hs in the Words experiment.

---
+++ src/Strappy/Experiments/Words/Grammar.hs
@@ -0,0 +1,23 @@
+
+
+module Strappy.Experiments.Words.Grammar where
+
+import Strappy.Expr
+import Strappy.Type
+import Strappy.Library
+import Strappy.Sample
+
+import Data.List (nub)
+
+cChar2Expr :: Char -> Expr
+-- | Convert char to expr
+cChar2Expr c = mkTerm (show c) tChar c
+
+alphabetExprs :: [Expr]
+alphabetExprs = map cChar2Expr ['a'..'z']
+
+wordsExprs :: [Expr]
+wordsExprs = nub $ basicExprs ++ listExprs ++ alphabetExprs
+
+wordsGr :: Grammar
+wordsGr = normalizeGrammar $ Grammar (log 0.45) (mkExprDistr wordsExprs) 
