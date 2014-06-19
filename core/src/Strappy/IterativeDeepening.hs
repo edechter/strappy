4d77d8d8f631fafad27f67233d5a12f9d6029b98
Eyal Dechter <eyaldechter@gmail.com>
2014-06-19 16:36:43 -0400

    IterativeDeepening.hs rewriting. Still broken.

--- core/src/Strappy/IterativeDeepening.hs
+++ core/src/Strappy/IterativeDeepening.hs
@@ -28,12 +28,12 @@ import Data.String (IsString)
 import Control.Monad.State
 import Data.Maybe
 
--- import Strappy.Utils
-import Strappy.Type hiding (unify, applySub, canUnifyWithSomeRightTree)
--- import qualified Strappy.Type as Type (unify, applySub, canUnifyWithSomeRightTree)
+import Strappy.Type 
 import Strappy.Expr
 import Strappy.Library
-import Strappy.Config
+import Strappy.Grammar
+
+import Numeric.StatsUtils
 
 import Control.DeepSeq
 import Criterion.Main
@@ -58,87 +58,13 @@ fromRight (Left err) = error $  "fromRight: " ++ err
 isRight (Right _) = True
 isRight _ = False
 
-seedGrammar :: Grammar
-seedGrammar = normalizeGrammar $ 
-                    Grammar { grApp = log 0.375,
-                        grExprDistr = Map.fromList
-                        [ (fromRight $ annotateRequested e, 0.0) | e <- numberExprs ] }
+infty :: Double
+infty = read "Infinity"
 
 _DEBUG = True
 debug s x = if _DEBUG then trace s x else x
 traceThis s x = trace (s ++ ": " ++ show x) x
 debugThis s x = if _DEBUG then traceThis s x else x
-
-infty = read ("Infinity") :: Double 
-
-runWithContext :: Monad m => Context -> TypeInference m a -> m (a, Context)
-{-# INLINE runWithContext #-}
-runWithContext (Context nTVar subst) m = do 
-       (a, (nTVar', subst')) <- runStateT m (nTVar, subst)
-       return (a, Context nTVar' subst')
-
-  --let (a, (nTVar', subst')) = runStateT m (nTVar, subst)
-  --                                       in (a, Context nTVar subst)
-
-unifyExpr :: Context -> Type -> Expr -> Either String Context
-{-# INLINE unifyExpr #-}
-unifyExpr (Context nTVar subst) t1 expr = flip evalStateT (nTVar, subst) $ do 
-    t <- instantiateType (eType expr)
-    Type.unify t1 t
-    (n, s) <- get 
-    return $ Context n s
-
-applySub :: Context -> Type -> (Type, Context)
-{-# INLINE applySub #-}
-applySub (Context nTVar subst) tp = (tp', Context nTVar' subst')
-  where (tp', nTVar', subst') = flip evalState (nTVar, subst) $ do 
-                                      tp' <- Type.applySub tp
-                                      (n, s) <- get
-                                      return (tp', n, s)
-
--- canUnifyWithSomeRightTree :: Context -> Type -> Type -> Bool
--- WithSomeRightTree (Context nTVar subst) t1 t2 = 
---   flip evalState (nTVar, subst) $ do 
---               t1' <- Type.applySub t1
---               t2' <- instantiateType t2 >>= Type.applySub
---               Type.canUnifyWithSomeRightTree t1' t2'
-
-
-cbSearch :: Grammar 
-         -> Double -- ^ Cost Bound
-         -> Type  
-         -> Context 
-         -> ([(Expr, Double, Context)], Double)
-cbSearch gr b tp ctx = if gamma > b 
-                          then (leaves, min gamma cMinL)
-                          else (applications, cMinA)
-  where 
-    gamma = negate $ grApp gr
-    !(leaves, cMinL) = loop [] infty $ map (second negate) . Map.toList $ (grExprDistr gr)
-          where loop out cMin [] = (out, cMin)
-                loop out cMin ((e, w):es) = 
-                  case unifyExpr ctx tp e of 
-                        Left _ -> loop out cMin es
-                        Right ctx' -> loop ((e, w, ctx'):out) 
-                                           (if w > b then min w cMin else cMin) es
-    (applications, cMinA) = loop leaves (gamma + k_l) lhss
-          where (eta, ctx') = freshTVar ctx
-                (lhss, k_l) = cbSearch gr (b - gamma) (eta ->- tp) ctx'
-
-                loop out cMin [] = (out, cMin)
-                loop out cMin ((eL, wL, ctxL):es) 
-                    | gamma + wL > b = loop out cMin es
-                    | otherwise      = loop out' cMin' es
-                    where (rhss, k_r) = cbSearch gr (b - gamma - wL) eta ctxL
-                          (out', cMin') = innerLoop out cMin rhss
-                            where innerLoop out cMin [] = (out, cMin)
-                                  innerLoop out cMin ((eR, wR, ctxR) : es) 
-                                      | gamma + wL + wR > b = innerLoop out cMin es
-                                      | otherwise  = innerLoop ((eApp, w, ctxR):out) cMin' es
-                                          where eApp = (App eL eR tApp Nothing $ Just (negate (gamma + wR + wL)))
-                                                w = gamma + wR + wL
-                                                tApp = fst $ applySub ctxR eta
-                                                cMin' = if gamma + wL + k_r > b then min cMin (gamma + wL + k_r) else cMin
 
 lowerBound :: Grammar -> Type -> Context -> Double -> Double 
 -- | Return a lower bound on the cost of a program of this type. 
@@ -152,86 +78,6 @@ lowerBound gr tp ctx@(Context nTVar subst) maxB = if null es
           minRecursiveCost = let (tp', ctx') = freshTVar ctx
                                  maxB' = maxB - negate (grApp gr)
                              in negate (grApp gr) + lowerBound gr (tp' ->- tp) ctx' maxB' + lowerBound gr tp' ctx' maxB'
-
-
-cbSearchV2 :: Grammar 
-         -> Double -- ^ Cost Bound
-         -> Type  
-         -> Context 
-         -> ([(Expr, Double, Context)], Double)
-cbSearchV2 gr b tp ctx = if gamma + wMin > b  
-                          then (leaves, min (gamma + wMin ) cMinL)
-                          else (applications, cMinA)
-  where 
-    wMin = minimum $ map (negate . snd) $ Map.toList $ (grExprDistr gr)
-    gamma = negate $ grApp gr
-    (leaves, cMinL) = loop [] infty $ map (second negate) . Map.toList $ (grExprDistr gr)
-          where loop out cMin [] = (out, cMin)
-                loop out cMin ((e, w):es) = 
-                  case unifyExpr ctx tp e of 
-                        Left _ -> loop out cMin es
-                        Right ctx' -> loop ((e, w, ctx'):out) 
-                                                                  (if w > b then min w cMin else cMin) es
-    (applications, cMinA) = loop leaves (gamma + k_l + wMin ) lhss
-          where (eta, ctx') = freshTVar ctx
-                (lhss, k_l) = cbSearchV2 gr (b - gamma - wMin ) (eta ->- tp) ctx'
-
-                loop out cMin [] = (out, cMin)
-                loop out cMin ((eL, wL, ctxL):es) 
-                    | gamma + wL > b = loop out cMin es
-                    | otherwise      = loop out' cMin' es
-                    where (rhss, k_r) = cbSearchV2 gr (b - gamma - wL) eta ctxL
-                          (out', cMin') = innerLoop out cMin rhss
-                            where innerLoop out cMin [] = (out, cMin)
-                                  innerLoop out cMin ((eR, wR, ctxR) : es) 
-                                      | gamma + wL + wR > b = innerLoop out cMin es
-                                      | otherwise  = innerLoop ((eApp, w, ctxR):out) cMin' es
-                                          where eApp = (App eL eR tApp Nothing $ Just (negate (gamma + wR + wL)))
-                                                w = gamma + wR + wL
-                                                tApp = fst $ applySub ctxR eta
-                                                cMin' = if gamma + wL + k_r > b then min cMin (gamma + wL + k_r) else cMin
-    --shortCircuit = not $ any (canUnifyWithSomeRightTree ctx tp) [eType e | e <- Map.keys $ grExprDistr gr]
-
-cbSearchV3 :: Grammar 
-         -> Double -- ^ Cost Bound
-         -> Type  
-         -> Context 
-         -> ([(Expr, Double, Context)], Double)
--- | Added a the lower bound for short circuiting. Major improvement in speed. 
-cbSearchV3 gr b tp ctx = if lb > b 
-                           then {-# SCC lb_gt_b_V3 #-} ([], lb)
-                           else if gamma + wMin > b 
-                                   then (leaves, min cMinL (gamma + wMin))
-                                   else (applications, cMinA)
-  where 
-    lb = lowerBound gr tp ctx b
-    wMin = minimum $ map (negate . snd) $ Map.toList $ (grExprDistr gr)
-    gamma = negate $ grApp gr
-    (leaves, cMinL) = loop [] infty $ map (second negate) . Map.toList $ (grExprDistr gr)
-          where loop out cMin [] = (out, cMin)
-                loop out cMin ((e, w):es) = 
-                  case unifyExpr ctx tp e of 
-                        Left _ -> loop out cMin es
-                        Right ctx' -> logToFile (show (e, w)) $ loop ((e, w, ctx'):out) cMin' es
-                          where cMin' = if w > b then min w cMin else cMin
-    (applications, cMinA) = loop leaves (gamma + k_l + wMin) lhss
-          where (eta, ctx') = freshTVar ctx
-                (lhss, k_l) = cbSearchV3 gr (b - gamma - wMin) (eta ->- tp) ctx'
-
-                loop out cMin [] = (out, cMin)
-                loop out cMin ((eL, wL, ctxL):es) 
-                    | gamma + wL > b = loop out cMin es
-                    | otherwise      = loop out' cMin' es
-                    where (rhss, k_r) = cbSearchV3 gr (b - gamma - wL) eta ctxL
-                          (out', cMin') = innerLoop out cMin rhss
-                            where innerLoop out cMin [] = (out, cMin)
-                                  innerLoop out cMin ((eR, wR, ctxR) : es) 
-                                      | gamma + wL + wR > b = innerLoop out cMin es
-                                      | otherwise  = logToFile (show (eApp, w)) $ innerLoop ((eApp, w, ctxR):out) cMin' es
-                                          where eApp = (App eL eR tApp Nothing $ Just (negate (gamma + wR + wL)))
-                                                w = gamma + wR + wL
-                                                tApp = fst $ applySub ctxR eta
-                                                cMin' = if gamma + wL + k_r > b then min cMin (gamma + wL + k_r) else cMin
 
 
 getUnifyingExprs :: Grammar -> Type -> Context -> [(Expr, Double, Context)]
@@ -301,7 +147,7 @@ cbIterativeDeepening :: Grammar
                      -> Type 
                      -> [(Expr, Double)]
 cbIterativeDeepening gr n tp = go 0 
-  where (tp', ctx') = runIdentity . runWithContext (Context 0 Map.empty) $ instantiateType tp
+  where (tp', ctx') = runIdentity . runStateT (Context 0 Map.empty) $ instantiateType tp
 
         go b = let (res, b') = cbSearchV4 gr b tp' ctx' []
                    k = length res
